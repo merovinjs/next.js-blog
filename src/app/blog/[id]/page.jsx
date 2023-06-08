d43dc5eab622da0ef28447d62c8d289a327e1e3a
Proprogam <k.kartal9595@gmail.com>
2023-06-08 20:51:08 +0300

    databaseadded

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import styles from "./page.module.css";
 import Image from "next/image";
 async function getData(id) {
-  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
+  const res = await fetch(`https://oldbee.netlify.app/${id}`, {
     cache: "no-cache",
   });
   if (!res.ok) {
