4bca9774ed000185e997cf39a56f9de41b9285ff
Proprogam <k.kartal9595@gmail.com>
2023-06-12 22:09:39 +0300

    register

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-import styles from "./FormPage.module.css";
+import styles from "./page.module.css";
 
 export default function FormPage() {
   const [title, setTitle] = useState("");
