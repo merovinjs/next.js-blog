f4dd41f82f49ab70b891e1dfd41fe4c6a0a46397
Proprogam <k.kartal9595@gmail.com>
2023-06-24 19:26:58 +0300

    footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,33 +1,41 @@
+"use client";
 import React from "react";
 import styles from "./page.module.css";
 import Image from "next/image";
+import { useContext } from "react";
+import { ThemeContext } from "@/context/ThemeContext";
 
 const Footer = () => {
+  const { theme } = useContext(ThemeContext);
+  const footerClassName =
+    theme === "light" ? styles.lightFooter : styles.darkFooter;
   return (
-    <div className={styles.container}>
-      <div>created @oldbeedev</div>
-      <div className={styles.imgContainer}>
-        <Image
-          src="/1.png"
-          className={styles.logo}
-          alt="oldbeedev facebook"
-          width={25}
-          height={25}
-        />
-        <Image
-          src="/2.png"
-          className={styles.logo}
-          alt="oldbeedev instagram"
-          width={25}
-          height={25}
-        />
-        <Image
-          src="/3.png"
-          className={styles.logo}
-          alt="oldbeedev twitter"
-          width={25}
-          height={25}
-        />
+    <div className={`${styles.wrapper} ${footerClassName}`}>
+      <div className={styles.container}>
+        <p className={styles.p}>created @oldbeedev</p>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/1.png"
+            className={styles.logo}
+            alt="oldbeedev facebook"
+            width={25}
+            height={25}
+          />
+          <Image
+            src="/2.png"
+            className={styles.logo}
+            alt="oldbeedev instagram"
+            width={25}
+            height={25}
+          />
+          <Image
+            src="/3.png"
+            className={styles.logo}
+            alt="oldbeedev twitter"
+            width={25}
+            height={25}
+          />
+        </div>
       </div>
     </div>
   );
